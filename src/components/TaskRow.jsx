738a9beca24231381c37e941875888179b711a69
FadiAlashqar <fadialshqar.fa01@gmail.com>
2025-08-25 18:24:48 +0200

    [UPD] milestone 3.4

--- src/components/TaskRow.jsx
+++ src/components/TaskRow.jsx
@@ -5,17 +5,21 @@ const TaskRow = () => {
 
     const { data, setData } = useContext(GlobalContext)
 
+    const status = [
+        "To do",
+        "Doing",
+        "Done"
+    ]
+
     return (
         <>
             {
                 data.map((d) => {
-                    return <>
-                        <tr key={d.id}>
-                            <td>{d.title}</td>
-                            <td>{d.status}</td>
-                            <td>{d.createdAt}</td>
-                        </tr >
-                    </>
+                    return <tr key={d.id}>
+                        <td>{d.title}</td>
+                        <td className={d.status === "To do" ? "text-danger" : d.status === "Doing" ? "text-warning" : "text-success"}>{d.status}</td>
+                        <td>{d.createdAt}</td>
+                    </tr >
 
                 })
             }
